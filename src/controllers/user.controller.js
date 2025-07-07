7207af4f672cbc5cb85e4d334f43aff773130de3
rayhankobir-dev <rayhankobir893@gmail.com>
2025-07-07 15:50:10 +0600

    fix: cookie issue

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -96,8 +96,8 @@ const loginUser = async (req, res) => {
     return res
       .cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
+        secure: process.env.ENVIRONMENT === "production",
+        sameSite: "node",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       })
       .json({ user, token });
