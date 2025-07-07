0b3e77ff13a96cd4ad9965d2f4725a98abb9e542
rayhankobir-dev <rayhankobir893@gmail.com>
2025-07-07 16:22:22 +0600

    fix: sameSite issue resolved

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -97,7 +97,7 @@ const loginUser = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         secure: process.env.ENVIRONMENT === "production",
-        sameSite: "none",
+        sameSite: process.env.ENVIRONMENT === "production" ? "none" : "lax",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       })
       .json({ user, token });
