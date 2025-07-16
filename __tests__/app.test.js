fc8e75c8ec1182ec6716511fe29f4445e685f051
rayhankobir-dev <rayhankobir893@gmail.com>
2025-07-16 09:11:32 +0600

    fix: write test cases

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../src/app");
 require("dotenv").config();
 
-describe("Basic Express Tests", () => {
+describe("API Routes", () => {
   it("should return 404 for unknown routes", async () => {
     await request(app).get("/nonexistent-route").expect(404);
   });
